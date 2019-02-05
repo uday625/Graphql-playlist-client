ecbde8f6953494fbc6e0f6db963d3e2beefffafd
UDAY BAG <uday625@Nemesis-2.local>
2019-02-05 11:43:32 -0500

    initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="main">
+          <h1> Uday's reading list</h1>
+          <BookList/>
+          <AddBook/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
